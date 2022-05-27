0f9febea0c531c53b67ce6c46f0b3da6e576d187
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-05-27 23:39:11 +0300

    another route (about page)

--- market.py
+++ market.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/") #decorators
 def hello_world():
-    return "<h1>Changed text! </h1>"
+    return "<h1>Hello World! </h1>"
+@app.route("/about/<username>") #decorators
+def about_page(username):
+    return f"<h1>This is the about page  of  {username}!</h1>"
