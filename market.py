95ac0c4a97ff5ac0c681f9b7ba9275f76cf4a11d
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-06-03 22:36:14 +0300

    listed items

--- market.py
+++ market.py
@@ -9,4 +9,11 @@ def home_page():
 
 @app.route("/market") 
 def market_page():
-    return render_template("market.html",item_name='Phone')    
+    items=[
+    {'id':1 ,'name':'Phone','barcode':'123456786','price':500},
+    {'id':2 ,'name':'Laptop','barcode':'987654321','price':1500},
+    {'id':3 ,'name':'Keyboard','barcode':'7684090585837','price':150}
+
+
+    ]
+    return render_template("market.html",items=items)    
