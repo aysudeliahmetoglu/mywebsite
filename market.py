6394697c5f7270161906cd90773d743718c77464
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-06-07 20:32:43 +0300

    SQLALchemy , model and database

--- market.py
+++ market.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1000), nullable=False, unique=True)
+
+
 
 
 @app.route("/")  # decorators
