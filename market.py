b97cc9ba1e1ac78042140d412e6f861b9cca9ebe
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-06-11 19:28:08 +0300

    edited

--- market.py
+++ market.py
@@ -3,4 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY']='dfd27c6c63080ec139990f7b'
 db = SQLAlchemy(app)
