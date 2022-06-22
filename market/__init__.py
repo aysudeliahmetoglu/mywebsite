bc446bff186533a3bd586bc1bb86fd39038c3290
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-06-22 23:32:28 +0300

    save password as password hash

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'dfd27c6c63080ec139990f7b'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
--- market/models.py
+++ market/models.py
@@ -1,12 +1,24 @@
 from market import db
+from market import Bcrypt
 
 class User(db.Model):
-     id = db.Column(db.Integer(),primary_key=True)
-     username = db.Column(db.String(length=30),nullable=False,unique=True)
-     email_address = db.Column(db.String(length=50),nullable=False,unique=True)
-     password_hash = db.Column(db.String(length=6),nullable=False) 
-     budget = db.Column(db.Integer(), nullable=False, default=1000)
-     items = db.relationship('Item', backref='owned_user', lazy=True)
+    id = db.Column(db.Integer(),primary_key=True)
+    username = db.Column(db.String(length=30),nullable=False,unique=True)
+    email_address = db.Column(db.String(length=50),nullable=False,unique=True)
+    password_hash = db.Column(db.String(length=6),nullable=False) 
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+    @property
+    def password(self):
+        return self.password
+
+
+
+    @password.setter
+    def password(self,plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
 
 
 class Item(db.Model):
@@ -16,7 +28,9 @@ class Item(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1000), nullable=False, unique=True)
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
-    
-
     def __repr__(self):
         return f'Item {self.name}'
+
+
+   
+   
