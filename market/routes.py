b7e32e754346a30e76c67407e2b2894c6c3e77fa
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-06-25 22:23:00 +0300

    login page continue

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for,flash 
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm,LoginForm
 from market import db
 
 
@@ -33,6 +33,7 @@ def register_page():
     return render_template('register.html', form=form)
 
 @app.route("/login",methods=['GET' , 'POST'])
-def login_page():    
-    return render_template('login.html')
+def login_page():   
+    form = LoginForm() 
+    return render_template('login.html',form=form)
     
