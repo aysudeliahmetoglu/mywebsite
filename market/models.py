1a92c4306d16f6d5fcc467a27ef38c04f4dea78f
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-07-02 14:25:15 +0300

    edited

--- market/models.py
+++ market/models.py
@@ -1,4 +1,4 @@
-from market import db,login_manager
+from market import db, login_manager
 from market import bcrypt
 from flask_login import UserMixin  #inheritance class
 
@@ -7,7 +7,7 @@ from flask_login import UserMixin  #inheritance class
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(),primary_key=True)
