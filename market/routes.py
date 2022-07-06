da4c2a2d64185aade852b8df3c09f5f1e4cc71f9
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2022-07-06 22:55:40 +0300

    conditions

--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash ,request
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm, PurchaseItemForm
 from market import db
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required,current_user
 
 
 @app.route("/")  # decorators
@@ -16,9 +16,17 @@ def home_page():
 @login_required
 def market_page():
     purchase_form = PurchaseItemForm()
-    if purchase_form.validate_on_submit():
-        print(request.form.get('purchased_item'))
-    items = Item.query.all()
+    if request.method == "POST":
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            if current_user.can_purchase():
+                p_item_object.owner = current_user.id
+                current_user.budget -= p_item_object.price
+                db.session.commit()
+                flash(f"Congrulations! You purchased {p_item_object.name} for {p_item_object.price}")
+    if request.method == "GET":
+        items = Item.query.filter_by(owner=None)
     
     
     return render_template("market.html", items=items,purchase_form=purchase_form)
